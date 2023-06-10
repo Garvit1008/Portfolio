086e291672cb7f2d0035a71e36705996cc39330c
Arihant <shuklaarihant0@gmail.com>
2023-06-10 12:10:07 +0530

    third commit

--- script.js
+++ script.js
@@ -1,26 +1,33 @@
 
 
-function navshow(){
+function nav_show(){
     const semi = document.getElementById('semi');
 const tog = document.getElementById('tog');
 const close = document.getElementById('close');
 
     tog.addEventListener("click",function(){
-        semi.classList.toggle('show');
-        close.classList.toggle('show');
-        tog.classList.toggle('hide');
+        semi.classList.remove('hide')
+        semi.classList.add('show');
+        close.classList.remove('hide')
+        close.classList.add('show');
+        tog.classList.remove('show')
+        tog.classList.add('hide');
     })
     
 }
 
-function hidden(){
+function hidden_nav(){
     const semi = document.getElementById('semi');
 const tog = document.getElementById('tog');
 const close = document.getElementById('close');
 
     close.addEventListener("click",function(){
-        semi.classList.toggle('hide');
-        close.classList.toggle('hide');
-        tog.classList.toggle('show');
+        semi.classList.remove('show');
+        semi.classList.add('hide');
+        close.classList.remove('show')
+        close.classList.add('hide');
+        tog.classList.remove('hide');
+        tog.classList.add('show');
     })
 }
+
