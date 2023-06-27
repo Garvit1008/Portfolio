2ce94d8184f505d4d330e685f07bd21b8cc049b5
Garvit1008 <92442314+Garvit1008@users.noreply.github.com>
2023-06-27 09:18:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,17 @@
-
-
-function nav_show(){
-    const semi = document.getElementById('semi');
-const tog = document.getElementById('tog');
-const close = document.getElementById('close');
-
-    tog.addEventListener("click",function(){
-        semi.classList.remove('hide')
-        semi.classList.add('show');
-        close.classList.remove('hide')
-        close.classList.add('show');
-        tog.classList.remove('show')
-        tog.classList.add('hide');
-    })
-    
-}
-
-function hidden_nav(){
-    const semi = document.getElementById('semi');
-const tog = document.getElementById('tog');
-const close = document.getElementById('close');
-
-    close.addEventListener("click",function(){
-        semi.classList.remove('show');
-        semi.classList.add('hide');
-        close.classList.remove('show')
-        close.classList.add('hide');
-        tog.classList.remove('hide');
-        tog.classList.add('show');
-    })
-}
-
+window.addEventListener('scroll',function(){
+    let a = window.scrollY;
+    if(a>=550&&a<=1600){
+        document.getElementById('skill-id').style.opacity = "1";
+        document.getElementById('skill-id').style.transition = "all 2s";
+    }
+    else{
+        document.getElementById('skill-id').style.opacity = "0";
+    }
+    });
+    function openForm() {
+        document.getElementById("myForm").style.display = "block";
+      }
+      
+      function closeForm() {
+        document.getElementById("myForm").style.display = "none";
+      } 
