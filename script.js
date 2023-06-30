d31c2459e55bb8d370dc37f1213dea64d859ce31
Garvit1008 <92442314+Garvit1008@users.noreply.github.com>
2023-06-30 20:03:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-window.addEventListener('scroll',function(){
-    let a = window.scrollY;
-    if(a>=550&&a<=1600){
-        document.getElementById('skill-id').style.opacity = "1";
-        document.getElementById('skill-id').style.transition = "all 2s";
-    }
-    else{
-        document.getElementById('skill-id').style.opacity = "0";
-    }
-    });
-    function openForm() {
-        document.getElementById("myForm").style.display = "block";
-      }
-      
-      function closeForm() {
-        document.getElementById("myForm").style.display = "none";
-      } 
+hamburger = document.querySelector(".hamburger");
+hamburger.onclick = function(){
+    navBar = document.querySelector(".nav-bar");
+    navBar.classList.toggle("active");
+}
+
+
+
